30ac32861f33be984fd31d1e78bea7c6b8a52e86
Eiromplays <Eiromplays@users.noreply.github.com>
2022-04-06 10:14:33 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Eiromplays"
+original_name="testing_flask"
+original_urlname="testing-flask"
+original_description="Awesome testing_flask created by Eiromplays"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv testing_flask $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for testing_flask package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("testing_flask", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="testing_flask",
+    version=read("testing_flask", "VERSION"),
+    description="Awesome testing_flask created by Eiromplays",
+    url="https://github.com/Eiromplays/testing-flask/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="Eiromplays",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["testing_flask = testing_flask.__main__:main"]
     },
     extras_require={
         "test": read_requirements("requirements-test.txt")
